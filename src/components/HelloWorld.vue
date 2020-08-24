a55aa508a29e55f0152c2fa572c56b8c3ca9e194
Nattapon Nimakul <totizy@gmail.com>
2020-08-24 14:46:54 +0700

    airdrop page poc

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,115 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>KUL Airdrop</h1>
+
+    <div v-if="account === ''">
+      <button @click="login">Login</button>
+    </div>
+    <div v-else>
+      <div>
+        <span>Account: </span><span>{{ account }}</span>
+      </div>
+
+      <p />
+
+      <div>
+        <span>Balance: </span><span>{{ balance }}</span>
+      </div>
+
+      <p />
+
+      <p />
+      <button @click="claim">Claim KUL Token</button>
+      <div v-if="toWait !== ''">
+        <span>wait: </span><span>{{ toWait }}</span>
+      </div>
+
+      <p />
+      <a v-if="txHash" :href="txHash">fsdf</a>
+    </div>
   </div>
 </template>
 
 <script>
+import * as nearAPI from "near-api-js"
+import getConfig from "../config"
+
+const nearConfig = getConfig(process.env.NODE_ENV || "development");
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      near: null,
+      walletAccount: null,
+      account: '',
+      contract: null,
+      balance: '',
+      toWait: '',
+      txHash: ''
+    }
+  },
+  methods: {
+    async login() {
+      this.walletAccount.requestSignIn(nearConfig.contractName, 'NEAR token example');
+    },
+    async connect() {
+      // Connect to network
+      this.near = await nearAPI.connect(Object.assign(nearConfig, { deps: { keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore() }}));
+      console.log(this.near)
+
+      // Account
+      this.walletAccount = new nearAPI.WalletAccount(this.near)
+      console.log(this.walletAccount)
+      console.log('account id', this.walletAccount.getAccountId())
+      this.account = this.walletAccount.getAccountId()
+
+      // Contract
+      this.contract = await this.near.loadContract(nearConfig.contractName, {
+        viewMethods: ['totalSupply', 'balanceOf', 'allowance', 'nextClaimTimestamp'],
+        changeMethods: ['init', 'transfer', 'approve', 'transferFrom', 'airdrop'],
+        sender: this.walletAccount.getAccountId()
+      });
+    },
+    async refresh() {
+      this.refreshBalance()
+      this.refreshToWait()
+    },
+    async refreshBalance() {
+      this.balance = await this.contract.balanceOf({tokenOwner: this.account})
+    },
+    async refreshToWait() {
+      const prev = await this.contract.nextClaimTimestamp({account: this.account})
+      if (prev != 0) {
+        const secondsPassed = (Date.now()-prev/1e6)/1e3
+        if (secondsPassed > 60) {
+          this.toWait = ''
+
+        } else {
+          this.toWait = `${parseInt(60 - secondsPassed)}s`
+        }
+        
+      } else {
+        this.toWait = ''
+      }
+    },
+    async claim() {
+      this.txHash = ''
+      const result = await this.contract.airdrop()
+      console.log(result)
+      this.txHash = `https://explorer.testnet.near.org/accounts/${this.account}`
+    }
+  },
+  mounted() {
+    this.connect().then(_ => {
+      setInterval(this.refresh, 1000);
+    })
+  },
+  created() {
+    console.log(nearAPI)
   }
 }
 </script>
